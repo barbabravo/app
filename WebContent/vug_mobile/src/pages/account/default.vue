74bde369fc5af63518604b32216971b7c07bb939
bluers <bluers@bluers-MacBook-Pro.local>
2017-02-21 14:51:03 +0800

    初始化

--- WebContent/vug_mobile/src/pages/account/default.vue
+++ WebContent/vug_mobile/src/pages/account/default.vue
@@ -26,89 +26,122 @@
 			</p>
 			<p>如有任何疑问，请向会员单位咨询。</p>
 			<div class="mint-cell mint-field">
-				<div class="mint-cell-left"></div> 
-					<div class="mint-cell-wrapper">
-						<div class="mint-cell-title">
-							<span class="mint-cell-text">开户邀请码</span> 
-						</div>
-						<div class="mint-cell-value">
-							<input placeholder="请输入用户名" type="text" class="mint-field-core">  
+				<div class="mint-cell-wrapper">
+					<div class="mint-cell-title"> 
+						<span class="mint-cell-text">开户邀请码</span> 
+					</div> 
+					<div class="mint-cell-value">
+						<input placeholder="请输入用户名" type="text" class="mint-field-core" maxlength="10">  
+						<div class="mint-field-clear" style="display: none;">
+							<i class="mintui mintui-field-error"></i>
+						</div> 
+						<span class="mint-field-state is-default"><i class="mintui mintui-field-default"></i></span> 
 						<div class="mint-field-other"></div>
 					</div>
-				</div> 
+				</div>  
 			</div>
 			<div class="identify-wrapper clearfix">
 				<div class="identify-part front">
 					<img src="../../public/images/identify-front.jpg" alt="">
 				</div>
-				<div class="identify-part back">
-					<img src="../../public/images/identify-back.jpg" alt="">
+				<div class="identify-part front">
+					<img src="../../public/images/identify-front.jpg" alt="">
 				</div>				
 			</div>
+			<mt-actionsheet
+			  :actions="actions"
+			  v-model="sheetVisible">
+			</mt-actionsheet>
 		</div>
 	</div>
 </template>
 
 <style lang="less">
-html{
-	font-size:62.5%;
-}
-body{
-	margin:0;
-	font-size:1.6rem;
-}
-.identify-wrapper{
-	display: table;
-	width:100%;
-	vertical-align:middle;
-	text-align: center;
-	padding:1rem;
-	box-sizing:border-box;
-}
-.identify-part{
-	display:table-cell;
-	background-color:#efefef;
-	border:1px solid #d4d4d4;
-	img{
-		width:10rem;
-		height:auto;
+	html{
+		font-size:62.5%;
 	}
-	&.front{
+	body{
+		margin:0;
+		font-size:1.6rem;
 	}
-	&.back{
+	.identify-wrapper{
+		display: table;
+		width:100%;
+		vertical-align:middle;
+		text-align: center;
+		padding:1rem;
+		box-sizing:border-box;
 	}
-}
+	.identify-part{
+		display:table-cell;
+		background-color:#efefef;
+		border:1px solid #d4d4d4;
+		padding:1rem;
+		img{
+			width:10rem;
+			height:auto;
+		}
+		&.front{
+		}
+		&.back{
+		}
+	}
 </style>
 
 <script>
-import 'mint-ui/lib/style.css'
-import '../../public/less/reset.less'
-import '../../public/less/base.less'
+import "mint-ui/lib/style.css";
+import "../../public/less/reset.less";
+import "../../public/less/base.less";
 
-import Vue 						from 'vue'
-import { Button, Cell, Toast} 	from 'mint-ui'
-import vuex 					from 'vuex'
-import vueRouter 				from 'vue-router'
+import Vue from 'vue'
+import {
+	Button, 
+	Cell, 
+	Toast, 
+	Indicator,
+	MessageBox,
+	Actionsheet
+} from 'mint-ui'
+import vuex from 'vuex'
+import vueRouter from 'vue-router'
 
 Vue.component(Button.name, Button)
 Vue.component(Cell.name, Cell)
+Vue.component(Actionsheet.name, Actionsheet);
 
 export default {
 	data(){
 		return {
-			a:1,
-			b:2
+			sheetVisible:false,
+			actions:[{
+				name:'aaa',
+				method:this.aaa
+			},{
+				name:'bbb',
+				method:this.bbb
+			}]
 		}
 	},
 	mounted(){
 		
 	},
 	methods:{
+		aaa(){
+			console.log('aaa')
+		},
+		bbb(){
+			console.log('bbb')
+		},
 		showToast(){
+			this.sheetVisible = true
+			MessageBox('提示', '操作成功');
 			let instance = Toast({
 			  message: '操作成功',
-			  iconClass: 'icon icon-error',
-			  duration:30000
+			  iconClass: 'icon icon-roundcheck'
+			});
+			Indicator.open({
+			  text: '加载中...',
+			  spinnerType: 'fading-circle'
 			});
 		}
 	}
